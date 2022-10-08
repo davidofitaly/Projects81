b92442f34d4300c757adcc8dc8b09d298337c4b5
davidofitaly <dwloch2@wp.pl>
2022-10-08 09:29:06 +0200

    #

---
+++ project_03/papier_kamien_nozycki.py
@@ -0,0 +1,5 @@
+player_1 = input('Podaj imie player_1: ')
+player_2 = input('Podaj imie player_2: ')
+
+player_1_odpowiedz = input(f'Hej {player_1} wybierz papier, kamien lub nozyczki: ')
+player_2_odpowiedz = input(f'Hej {player_2} wybierz papier, kamien lub nozyczki: ')
